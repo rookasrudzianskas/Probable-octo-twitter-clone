72aa7cd366f782a84196060d2e39d3b82ca1f65e
Rokas <rookas.rudzenskas@gmail.com>
2021-05-05 17:32:51 +0300

    Rokas

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -2,9 +2,9 @@ import React from 'react';
 import "./styles/SidebarOption.css";
 
 
-const SidebarOption = ({ text, Icon }) => {
+const SidebarOption = ({ active, text, Icon }) => {
     return (
-        <div className="sidebarOption">
+        <div className={`sidebarOption ${active && 'sidebarOption--active'}`}>
             <Icon />
             <h2>{text}</h2>
         </div>

2bfa61550f243900c919524244b1dc982c8e3c52
Rokas <rookas.rudzenskas@gmail.com>
2021-05-05 17:03:57 +0300

    Rokas

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import "./styles/Sidebar.css";
-
+import TwitterIcon from "@material-ui/icons/Twitter";
+import SidebarOption from "./SidebarOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import ListAltIcon from "@material-ui/icons/ListAlt";
+import PermIdentityIcon from "@material-ui/icons/PermIdentity";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import { Button } from "@material-ui/core";
 const Sidebar = () => {
     return (
         <div className="sidebar">
--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,16 +1,6 @@
 import React from 'react';
 import "./styles/SidebarOption.css";
-import TwitterIcon from "@material-ui/icons/Twitter";
-import SidebarOption from "./SidebarOption";
-import HomeIcon from "@material-ui/icons/Home";
-import SearchIcon from "@material-ui/icons/Search";
-import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
-import MailOutlineIcon from "@material-ui/icons/MailOutline";
-import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
-import ListAltIcon from "@material-ui/icons/ListAlt";
-import PermIdentityIcon from "@material-ui/icons/PermIdentity";
-import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
-import { Button } from "@material-ui/core";
+
 
 const SidebarOption = () => {
     return (

5d2715a1ca718996c78c2fd295ccc8f8ec8cda20
Rokas <rookas.rudzenskas@gmail.com>
2021-05-06 06:23:28 +0300

    Roaks

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import "./styles/Widgets.css";
+import SearchIcon from "@material-ui/icons/Search";
 
 const Widgets = () => {
     return (
         <div className="widgets">
-            <h2>Rokaks</h2>
+            <div className="widgets__input">
+                <SearchIcon className="widgets__searchIcon" />
+                <input type="text" placeholder="Search Twitter"/>
+            </div>
+
+            <div className="widgets__widgetContainer">
+                <h2>What's Happening</h2>
+            </div>
         </div>
     );
 };
